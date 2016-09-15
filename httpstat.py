3669fff592e29421b8993dffacd9411a4813590d
jyz5257 <jyz5257@bu.edu>
2016-09-15 15:21:25 -0400

    color change and underline

--- httpstat.py
+++ httpstat.py
@@ -82,7 +82,7 @@ cyan = make_color(36)
 bold = make_color(1)
 underline = make_color(4)
 
-grayscale = {(i - 232): make_color('38;5;' + str(i)) for i in xrange(232, 256)}
+grayscale = {(i - 232): make_color('4;31;' + str(i)) for i in xrange(232, 256)}
 
 
 def quit(s, code=0):
